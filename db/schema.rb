5c9e3feb3d9206fdf5aa0fa6eeee8cb7fe93c7fe
akhror-valiev <ahror.valiev@gmail.com>
2023-05-12 16:55:24 +0500

    Fix conflicts and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_052251) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_08_144230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,10 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_052251) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-ActiveRecord::Schema[7.0].define(version: 2023_05_08_144230) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name", default: "", null: false
@@ -40,7 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_144230) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
