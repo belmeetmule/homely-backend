a69ca305d61da2fa329706af8dab535a52f52282
akhror-valiev <ahror.valiev@gmail.com>
2023-05-12 23:00:00 +0500

    Update crud api endpoints

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,42 +1,54 @@
 class Api::V1::HousesController < ApplicationController
   def index
-    @houses = House.all
-    render json: @houses, status: :ok
+    houses = House.all
+    render json: houses, status: 200
   end
 
   def show
-    house = House.find(params[:id])
-    render json: house
+    house = House.find_by(id: params[:id])
+    if house
+      render json: house, status: 200
+    else
+      render json: {
+        error: 'House not found'
+      }
+    end
   end
 
   def create
-    house = House.new(house_params)
-
+    house = House.new(
+      name: house_params[:name],
+      city: house_params[:city],
+      image: house_params[:image],
+      appartment_fee: house_params[:appartment_fee],
+      description: house_params[:description]
+    )
     if house.save
-      render json: house, status: :created
+      render json: house, status: 200
     else
-      render json: house.errors, status: :unproccessable_entity
+      render json: {
+        error: 'Error Creating...'
+      }
+    end
+  end
+
+  def destroy
+    house = House.find_by(id: params[:id])
+    if house
+      house.destroy
+      render json: 'House has been deleted'
     end
   end
 
   private
 
   def house_params
-    params.require(:house).permit(:name, :city, :image, :appartment_fee, :description)
-  end
-
-  def destroy
-    house = House.find(params[:id])
-    if house
-      house.destroy
-      render json: {
-        status: 200,
-        message: "Successfully removed #{house.name}"
-      }
-    else
-      render json: {
-        error: mentor.errors.full_message
-      }
-    end
+    params.require(:house).permit(%i[
+                                    name
+                                    city
+                                    image
+                                    appartment_fee
+                                    description
+                                  ])
   end
 end
