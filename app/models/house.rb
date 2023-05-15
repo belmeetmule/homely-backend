b1c7043acb9fb411b80c2d597000ec8b1cea19ba
akhror-valiev <ahror.valiev@gmail.com>
2023-05-15 20:12:19 +0500

    Add validations

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,8 @@
 class House < ApplicationRecord
   has_many :users
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :image, presence: true
+  validates :appartment_fee, presence: true
+  validates :description, presence: true
 end
