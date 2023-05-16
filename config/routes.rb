0052e1c727aaad5b1297a6300c0c880ee6b17868
mule <belmeetmule@gmail.com>
2023-05-16 12:54:25 +0300

    remove commnted code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get '/current_user', to: 'current_user#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/current_user', to: 'current_user#index'
 
     devise_for :users, path: '', path_names: {
       sign_in: 'login',
