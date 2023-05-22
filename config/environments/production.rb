7cda70ef93bd1bc30e26a0fb7fd4871678f249b7
mule <belmeetmule@gmail.com>
2023-05-22 15:50:50 +0300

    remove asset:precompile from render-build script

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.assets.compile = true
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
